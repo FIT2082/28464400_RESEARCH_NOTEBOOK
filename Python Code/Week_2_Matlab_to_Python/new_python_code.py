eaaa6f79b5ab436c817b3fe8e1a729b67a199bb5
surayez <surayez@yahoo.com>
2018-08-05 14:33:32 +1000

    Initial Commit

---
+++ Python Code/Week_2_Matlab_to_Python/new_python_code.py
@@ -0,0 +1,34 @@
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plot
+
+def find_edge(userData):
+    for days in range(0, 365):
+        current_day_array = []
+        for hours in range(0,48):
+            current_day_array.append(matrix[hours][days])
+        current_day_sd = np.std(current_day_array)
+
+        for hours in range(0,48):
+            if matrix[hours][days] - matrix[hours-1][days] >= current_day_sd:
+                matrix[hours][days] = 3
+            # if matrix[hours+1][days] - matrix[hours][days] >= current_day_sd:
+            #     matrix[hours][days] = 3
+
+    for days in range(0, 364):
+        for hours in range(0, 48):
+            if matrix[hours][days] == 3 and matrix[hours][days-1] == 3:
+                matrix[hours][days] = 10
+                matrix[hours][days-1] = 10
+
+
+data = scipy.io.loadmat('matlab.mat')
+matrix = data['image']
+
+#convert data into array (each array = 1 time slot, index(item) = day
+
+# print(np.squeeze(np.matrix(matrix)))
+find_edge(matrix)
+
+plot.pcolor(matrix) #plot
+# plot.show()
